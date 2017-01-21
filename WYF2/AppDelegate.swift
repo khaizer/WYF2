202027adf32720074da6bc74a8f3ef8d2a945e2d
Khai Nguyen <khaizer@Khais-Mac.local>
2017-01-21 13:29:05 -0800

    Completed email auth

--- WYF2/AppDelegate.swift
+++ WYF2/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Firebase
-
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FIRApp.configure()
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         return true
     }
@@ -45,7 +47,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
+    func application(_ application:UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 
 }
 
--- WYF2/SignInVC.swift
+++ WYF2/SignInVC.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var passwordField: FancyField!
+    @IBOutlet weak var emailField: FancyField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,53 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) {(result, error) in
+        if error != nil {
+                print("KHAI: Unable to authenticate with Facebook -\(error)")
+        } else if result?.isCancelled == true {
+                print("KHAI: User cancelled Facebook authentication")
+        } else {
+                print("KHAI: Successfully authenticated with Facebook")
+            let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+            
+            self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("KHAI: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("KHAI: Successfully authenticated with Firebase")
+            }
+        })
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let password = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion:{ (user, error) in
+                if error == nil {
+                    print("KHAI: Email user authenticated with Firebase")
+                } else {
+            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                if error != nil {
+                    print("KHAI: Unable to authenticate with Firebase using email")
+                } else {
+                    print("KHAI: Successfully authenticated with Firebase")
+                }
+                    
+                    })
+                }
+            })
+        }
+    }
+    
 }
 

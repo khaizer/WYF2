dbe97a6e70cdce9067d3dfa8a77b593654d3e888
Khai Nguyen <khaizer@Khais-Mac.local>
2017-01-20 23:16:45 -0800

    Sign in ui custom classes completed

---
+++ WYF2/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  WYF2
+//
+//  Created by Khai Nguyen on 1/20/17.
+//  Copyright © 2017 KhaizerkhaiStudios. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 225.0
